7c832ae2425b61865cd9fa3ee43e81eeb26d92ee
Osk <olesea.skutnitki@usarb.md>
2021-03-24 21:55:33 +0200

    A fost adaugata clasa Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+package edu.tekwill.java;
+
+public class Main {
+    public static void main (String [] args){
+        /*Clasa1 ob=new Clasa1();
+        ob.nr=1;
+        System.out.println("Hello Tekwill");
+        ob.afisareSalut();*/
+    }
+}
