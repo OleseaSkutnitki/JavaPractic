a02ae7e22d1ca01b99a2aca985cb1fd6e94fdccd
Osk <olesea.skutnitki@usarb.md>
2021-03-31 18:08:22 +0300

    Cod modificat

--- Ex1/src/edu/tekwill/java/Main.java
+++ Ex1/src/edu/tekwill/java/Main.java
@@ -1,10 +1,52 @@
 package edu.tekwill.java;
+import java.util.*;
 
 public class Main {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         /*Clasa1 ob=new Clasa1();
         ob.nr=1;
         System.out.println("Hello Tekwill");
         ob.afisareSalut();*/
+        /*int intVariable = 34;
+        float floatVar = 23.5f;
+        double doubleVar = 34.5;
+        boolean booleanVar = true;
+        System.out.println(intVariable);
+        System.out.println(floatVar);
+        System.out.println(doubleVar);
+        System.out.println(booleanVar);
+        String name = "Olesea";
+        String surname = "Skutnitki";
+        String message;
+        message = "My name is " + name + " " + surname;
+        System.out.println(message);
+        double price=65.4;
+        double tax=13;
+        int cuanti=4;
+        double total;
+        String message1="I want to buy "+cuanti+" books";
+        total=price*cuanti*tax;
+        String mess2="Total coast is "+total;
+        System.out.println(message1);
+        System.out.println(mess2);
+        Scanner sc=new Scanner(System.in);
+        String name1;
+        String surname1;
+        System.out.println("Nr");
+        int nr=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Introdu numele");
+        name1=sc.nextLine();
+        System.out.println("Introdu prenumele");
+        surname1=sc.nextLine();*/
+        Scanner sc=new Scanner(System.in);45
+        double nr1,nr2,sum,prod,dif,imp;
+        System.out.println("First number");
+        nr1=sc.nextDouble();
+        System.out.println("Second number");
+        nr2=sc.nextDouble();
+        sum=nr1+nr2;
+        System.out.println("Sum of "+nr1+" and "+nr2+" is "+sum);
+
     }
 }
